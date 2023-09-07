606a9b338c47a532f713e7c045094e25ba3ac700
ADRIAN NICOLAE BRICIU <60313520+BriciuAdrian83@users.noreply.github.com>
2023-09-07 10:12:23 +0300

    update middle of containers and dependencies

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass;
+use Framework\Exceptions\ContainerException;
+
 class Container
 {
     private array $definitions = [];
 
     public function addDefinitions(array $newDefinitions)
     {
-        // $this->definitions = [...$this->definitions, ...$newDefinitions];
         $this->definitions = array_merge($this->definitions, $newDefinitions);
-        dd($newDefinitions);
+    }
+
+    public function resolve(string $className)
+    {
+        $reflectionClass = new ReflectionClass($className);
+
+        if (!$reflectionClass->isInstantiable()) {
+            throw new ContainerException("The class {$className} is not an iterable class");
+        }
+
+        dd($reflectionClass);
     }
 }
